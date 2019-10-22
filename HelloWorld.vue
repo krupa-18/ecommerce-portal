2708bf2bde2d718081b4fea5457f6354a8a5fa21
krupa-18 <55389224+krupa-18@users.noreply.github.com>
2019-10-22 19:39:56 +0530

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="HelloWorld">
+    <div align='center'>
+      <div id="search">
+        <input v-model="search" placeholder="search products by name">      
+          <router-link  :to="{ name: 'Search', params: { term: search }}">
+             <button type="button" class= "button" >Search</button>
+          </router-link>          
+      </div>
+      <ul v-bind:key="item.id" v-for="item in list">
+        <img v-if="item.image" :src=" '../assests/' + item.image" />
+          {{ item.name }}
+          {{ item.price }}
+    </ul>    
+      </div>
+  </div>
+</template>
+  
+
+<script>
+
+  import Search from './Search';
+  import axios from 'axios';
+  export default {
+    name: 'HelloWorld',
+    
+    data () {
+      return {
+        list: {},
+        search: '',
+      }
+    },
+    components: {
+      Search
+    },
+    mounted()
+    {
+      axios.get('http://localhost:4000/mostpopular')
+      .then((response) => {
+        console.log(response.data);
+        this.list = response.data;
+      })
+      .catch((error) =>{
+        console.log(error);
+      });
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+img {
+  max-width: 300px;
+  height: auto;
+}
+
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 5px 5px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
